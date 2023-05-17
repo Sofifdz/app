aeefbbd708147c35db0749b83ad1b67325e5ddd9
sf <chofi.sofiamarianana@gmail.com>
2023-05-16 19:07:19 -0600

    Punto de venta

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:app/categoria.dart';
+import 'package:app/proveedor.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:app/producto.dart';
+import 'package:app/home.dart';
+
+void main() async {
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(ProductoAdapter());
+  var boxp = await Hive.openBox('productos');
+  Hive.registerAdapter(CategoriaAdapter());
+  var boxc = await Hive.openBox('categorias');
+  Hive.registerAdapter(ProveedorAdapter());
+  var boxproveedor = await Hive.openBox('proveedor');
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.cyan,
+      ),
+      home: Home(),
+    );
+  }
+}
